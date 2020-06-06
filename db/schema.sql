d92f5bd480e0bc97744af323ff2e6f58806f022c
J Fav <jfavorito@outlook.com>
2020-06-06 10:19:44 -0700

    added some code

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS projecttwo_db;
-CREATE DATABASE projecttwo_db;
+DROP DATABASE IF EXISTS user_db;
+CREATE DATABASE user_db;
---
+++ models/account.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const Account = sequelize.define(
+        "account",
+        {
+            user_name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        },
+        {
+            freezeTableName: true
+        }
+    );
+
+    Account.associate = (models) => {
+        Account.hasOne(models.employee, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return Account;
+};
