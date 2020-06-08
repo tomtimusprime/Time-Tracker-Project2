400bde48cde98e613a3221700a70602926f65bfa
J Fav <jfavorito@outlook.com>
2020-06-08 16:14:29 -0700

    added document.ready to users.js

--- public/js/users.js
+++ public/js/users.js
@@ -1,42 +1,46 @@
-const addUser = async (name, wage) => {
-    let newUser = {
-        full_name: name,
-        time_worked: 0,
-        wage: wage,
-        total_time: 0,
-        total_earnings: 0
+$(document).ready(function () {
+
+
+    const addUser = async (name, wage) => {
+        let newUser = {
+            full_name: name,
+            time_worked: 0,
+            wage: wage,
+            total_time: 0,
+            total_earnings: 0
+        }
+        await
+            $.ajax("/api/users/" + id, {
+                type: "POST",
+                data: newUser
+            }).then(
+                function () {
+                    console.log("Added user: ", newUser);
+                    location.reload();
+                }
+            );
     }
-    await
-        $.ajax("/api/users/" + id, {
-            type: "POST",
-            data: newUser
-        }).then(
-            function () {
-                console.log("Added user: ", newUser);
-                location.reload();
-            }
-        );
-}
 
-const updateUser = async (timeWorked, totalTime, totalEarnings) => {
-    var newUserData = {
-        time_worked: timeWorked,
-        total_time: totalTime,
-        total_earnings: totalEarnings
-    };
+    const updateUser = async (timeWorked, totalTime, totalEarnings) => {
+        var newUserData = {
+            time_worked: timeWorked,
+            total_time: totalTime,
+            total_earnings: totalEarnings
+        };
 
-    await
-        $.ajax("/api/users/" + id, {
-            type: "PUT",
-            data: newUserData
-        }).then(
-            function () {
-                console.log("Updated user data: ", newUserData);
+        await
+            $.ajax("/api/users/" + id, {
+                type: "PUT",
+                data: newUserData
+            }).then(
+                function () {
+                    console.log("Updated user data: ", newUserData);
 
-                location.reload();
-            }
-        );
-}
+                    location.reload();
+                }
+            );
+    }
 
-exports.addUser = addUser;
-exports.updateUser = updateUser;
+    exports.addUser = addUser;
+    exports.updateUser = updateUser;
+});
