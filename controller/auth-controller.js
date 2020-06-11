12b46a85e8f93cbf487c8dced4b9e2ea0053320a
Dazacher <dazacher2000@yahoo.com>
2020-06-11 13:43:25 -0700

    Changed back cookie max age to default value.

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -12,7 +12,7 @@ const path = require("path");
 // Flash
 router.use(
   session({
-    cookie: { maxAge: 600000 },
+    cookie: { maxAge: 60000 },
     secret: "wootwoot",
     saveUninitialized: true,
     resave: true
